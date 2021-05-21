74cbfe83adf2c3dc6540c9b248de7c3bbee3c1b7
Lokesh <40594677+Imloki45@users.noreply.github.com>
2021-05-21 19:23:21 +0530

    Update settings.py

--- ToDo/settings.py
+++ ToDo/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = '454545loki454545'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["pythonanywhere.com"]
 
 
 # Application definition
